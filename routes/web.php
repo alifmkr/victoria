23aac125b338604c4f08b28e6dcb2c14fcc853b7
midrisssss <muhmmadidris15@gmail.com>
2024-09-30 12:45:46 +0700

    Add view

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 // Route halaman login
 Route::get('/login', function () {
-    return 'Ini Login';
+    return view('/login');
 });
 
 // redirect halaman ketika halaman logout di akses halaman login
@@ -28,25 +28,71 @@ Route::redirect('/logout', '/login');
 // page only for admin
 Route::prefix('/admin')->group(function () {
 
-    Route::get('/home', function () {
-        return 'Ini Home';
+    Route::get('/dashboard', function () {
+        $page = array(
+            "page" => "Dashboard",
+            "description" => "Halaman yang berisi ringkasan aktivitas harian, seperti total penjualan, jumlah transaksi, produk terlaris, dan performa penjualan dalam periode tertentu."
+        );
+        return view('/dashboard')->with("page", $page);
     });
 
-    Route::get('/program', function () {
-        return 'Ini Program';
+    Route::get('/pos', function () {
+        $page = array(
+            "page" => "Point of Sales",
+            "description" => "Halaman inti untuk melakukan transaksi pembelian. Fitur utama termasuk pencarian produk, total pembelian, dan metode pembayaran. Halaman ini memungkinkan kasir untuk menyelesaikan transaksi dengan cepat dan efisien.",
+        );
+
+        return view('/pos')->with('page', $page);
     });
 
-    Route::get('/about', function () {
-        return 'Ini About';
+    Route::get('/inventory', function () {
+        $page = array(
+            "page" => "Inventory",
+            "description" => "Halaman untuk mengelola stok produk. Admin dapat menambah, mengedit, atau menghapus produk, serta memantau jumlah stok secara real time.",
+        );
+        return view('/inventory')->with("page", $page);
     });
 
-    Route::get('/contactus', function () {
-        return 'Ini Contact Us';
+    Route::get('/transactions', function () {
+        $page = array(
+            "page" => "Transactions Management",
+            "description" => "Halaman yang menyimpan semua riwayat transaksi yang pernah dilakukan. Kasir dan admin dapat mencari dan melihat detail setiap transaki, termasuk item yang dibeli, jumlah yang dibayar, metode pembayaran, dan waktu transaksi.",
+        );
+        return view('/transactions')->with("page", $page);
     });
 
-    Route::get('/home/{name?}', function ($name = 'Jhonee') {
-        return 'Ini ' . $name;
+    Route::get('/user', function () {
+        $page = array(
+            "page" => "User Management",
+            "description" => "Halaman untuk mengelola akun pengguna sistem, seperti kasir dan admin.  Admin dapat menambah, mengedit, atau menghapus akun pengguna serta menetapkan hak akses user lain."
+        );
+        return view('/user')->with("page", $page);
     });
+
+    Route::get('/settings', function () {
+        $page = array(
+            "page" => "Settings",
+            "description" => "Halaman untuk mengatur preferensi sistem, seperti mata uang, pajak, default, metode pembyaran yag diterima, dan pengaturan lainnya yang mendukung operasiobal kasir.",
+        );
+        return view('/settings')->with("page", $page);
+    });
+
+    Route::get('/notifications', function () {
+        $page = array(
+            "page" => "Notifications",
+            "description" => "Halaman untuk melihat notifikasi terbaru seperti stok produk yang menipurs, transaksi besar, atau perubahan penting pada sistem. Notifikasi ini membantu admin untuk selalu waspada terhadapa aktivitas yang memerlukan tindakan.",
+        );
+        return view('/notifications')->with("page", $page);
+    });
+
+    Route::get('/help', function () {
+        $page = array(
+            "page" => "Help and Support",
+            "description" => "Halaman yang menyediakan panduan penggunaan sistem kasir, FAQ, dan cara menghubungi tim support jika pengguna mengalami masalah dengan sistem."
+        );
+        return view('/help')->with("page", $page);
+    });
+
 });
 
 // halaman 404
